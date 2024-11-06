8513ad0431d57891ad52b026efaa3d24fd9c8af2
XaquinAlves <xaquinalves@gmail.com>
2024-11-06 16:42:11 +0100

    Second Commit(actv3.1)

--- UD3/actividade3a1a3/Statistics.java
+++ UD3/actividade3a1a3/Statistics.java
@@ -9,15 +9,24 @@ import java.util.Scanner;
  * @author Xaquin Alves Gonzalez
  */
 public class Statistics {
-    private int[] numberList;
 
     /**
      * Constructor que pide por teclado a cantidade de números e os números en
      * si
      */
     public Statistics() {
+    }
 
-        numberList = getNumbers();
+    /**
+     * Creamos unha instancia de Statistics para testeala
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Statistics stats = new Statistics();
+
+        System.out.println("La mediana es : " + stats.median());
     }
 
     private int[] getNumbers() {
@@ -33,7 +42,7 @@ public class Statistics {
             length = scan.nextInt();
             scan.nextLine();
         }
-        int[] numberList=new int[length];
+        int[] numberList = new int[length];
 
         for (int i = 0; i < length; i++) {
             System.out.print("Introduce el número nº " + i + ": ");
@@ -45,54 +54,34 @@ public class Statistics {
     }
 
     /**
-     * Creamos unha instancia de Statistics para testeala
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-
-        Statistics stats = new Statistics();
-
-        System.out.println("La mediana es : " + stats.median());
-    }
-
-    /**
      * @return a mediana dos números gardados
      */
     public int median() {
+        int minors, greaters, median = 0;
 
-        int[] minors = new int[(numberList.length - 1) / 2];
-        int[] greaters = new int[(numberList.length - 1) / 2];
-        int result = numberList[(numberList.length - 1) / 2];
-        //Gardamos a metade dos numeros nun array
-        for (int i = 0; i < (numberList.length - 1) / 2; i++) {
-            minors[i] = numberList[i];
-        }
-        //Gardamos a outra metade dos numeros(deixando 1 fora de ambos arrays en result
-        for (int i = ((numberList.length - 1) / 2) + 1, j = 0; i < numberList.length; i++, j++) {
-            greaters[j] = numberList[i];
-        }
-        //Pomos no array minors os menores e no greaters os maiores
-        for (int i = 0; i < minors.length; i++) {
-            if (minors[i] > greaters[i]) {
-                int temp = minors[i];
-                minors[i] = greaters[i];
-                greaters[i] = temp;
+        int[] numberList = getNumbers();
+
+        for (int i : numberList) {
+
+            minors = 0;
+            greaters = 0;
+
+            for (int j : numberList) {
+
+                if (i < j) {
+                    greaters++;
+                } else if (i > j) {
+                    minors++;
+                } else {
+                    minors++;
+                    greaters++;
+                }
+            }
+
+            if (minors == greaters) {
+                median = i;
             }
         }
-        //Comparamos o numero deixado fora cos dous arrays pa obter a mediana
-        for (int i = 0; i < minors.length; i++) {
-            if (minors[i] > result) {
-                int temp = minors[i];
-                minors[i] = result;
-                result = temp;
-            } else if (greaters[i] < result) {
-                int temp = greaters[i];
-                greaters[i] = result;
-                result = temp;
-            }
-        }
-
-        return result;
+        return median;
     }
 }
