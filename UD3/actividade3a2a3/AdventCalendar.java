044ca7027b13ae2dbc570a30e0c7b460e4a44bba
XaquinAlves <xaquinalves@gmail.com>
2024-11-07 10:22:23 +0100

    First Commit(actv3.2)

--- UD3/actividade3a2a3/AdventCalendar.java
+++ UD3/actividade3a2a3/AdventCalendar.java
@@ -6,7 +6,7 @@ package UD3.actividade3a2a3;
 public class AdventCalendar {
     private final int MAX_X = 6;
     private final int MAX_Y = 4;
-    private final int MAX_VALUE =25;
+    private final int MAX_VALUE = 25;
     private int matrix[][];
 
     /**
@@ -24,7 +24,7 @@ public class AdventCalendar {
     }
 
     /**
-     *
+     * Mostra o calendario de advento por pantalla
      */
     public void show() {
         for (int i = 0; i < MAX_X; i++) {
@@ -35,22 +35,67 @@ public class AdventCalendar {
         }
     }
 
+    /**
+     * Enche a matriz con valores aleatorios de 1 a 24
+     */
     public void fill() {
         for (int i = 0; i < MAX_X; i++) {
             for (int j = 0; j < MAX_Y; j++) {
                 do {
                     matrix[i][j] = new java.util.Random().nextInt(MAX_VALUE);
-                }while (matrix[i][j] == 0);
+                } while (matrix[i][j] == 0);
             }
         }
     }
 
+    /**
+     * Come un elemento (o de menor numero)
+     */
     public void eat() {
+        int minor = Integer.MAX_VALUE, x = 0, y = 0;
 
+        for (int i = 0; i < MAX_X; i++) {
+            for (int j = 0; j < MAX_Y; j++) {
+                if (matrix[i][j] < minor && matrix[i][j] != 0) {
+                    minor = matrix[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        matrix[x][y] = 0;
     }
 
+    /**
+     * Comproba se e Navidade vendo se todos os elementos valen 0
+     *
+     * @return se é Navidade
+     */
     public boolean christmasIsHere() {
+        boolean isChristmas = true;
 
+        for (int i = 0; i < MAX_X && isChristmas; i++) {
+            for (int j = 0; j < MAX_Y && isChristmas; j++) {
+                if (matrix[i][j] != 0) {
+                    isChristmas = false;
+                }
+            }
+        }
+
+        return isChristmas;
     }
 
+    public static void main(String[] args) {
+        AdventCalendar calendar = new AdventCalendar();
+        calendar.fill();
+        System.out.println("Este es el calendario: ");
+        calendar.show();
+
+        while (!calendar.christmasIsHere()){
+            System.out.println("Voy a comer uno");
+            calendar.eat();
+            calendar.show();
+        }
+    }
 }
