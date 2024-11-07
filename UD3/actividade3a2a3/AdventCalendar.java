628a695c780dce492207e2163bacb347b521fe34
XaquinAlves <xaquinalves@gmail.com>
2024-11-07 11:20:46 +0100

    Second Commit(actv3.2)

--- UD3/actividade3a2a3/AdventCalendar.java
+++ UD3/actividade3a2a3/AdventCalendar.java
@@ -4,7 +4,7 @@ package UD3.actividade3a2a3;
  * @author Xaquin Alves Gonzalez
  */
 public class AdventCalendar {
-    private final int ROWS = 6;
+    private final int ROW = 6;
     private final int COLUMNS = 4;
     private final int MAX_VALUE = 25;
     private int[][] matrix;
@@ -13,7 +13,26 @@ public class AdventCalendar {
      * Crea unha instancia de AdventCalendar
      */
     public AdventCalendar() {
-        matrix = new int[ROWS][COLUMNS];
+        matrix = new int[ROW][COLUMNS];
+
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AdventCalendar calendar = new AdventCalendar();
+        calendar.fill();
+        System.out.println("Este es el calendario: ");
+        calendar.show();
+
+        while (!calendar.christmasIsHere()) {
+            System.out.println("Voy a comer uno");
+            calendar.eat();
+            calendar.show();
+        }
     }
 
     /**
@@ -27,7 +46,7 @@ public class AdventCalendar {
      * Mostra o calendario de advento por pantalla
      */
     public void show() {
-        for (int i = 0; i < ROWS; i++) {
+        for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUMNS; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
@@ -39,12 +58,14 @@ public class AdventCalendar {
      * Enche a matriz con valores aleatorios de 1 a 24
      */
     public void fill() {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMNS; j++) {
-                do {
-                    matrix[i][j] = new java.util.Random().nextInt(MAX_VALUE);
-                } while (matrix[i][j] == 0);
-            }
+        int x, y;
+
+        for (int i = 1; i < MAX_VALUE; i++) {
+            do {
+                x = new java.util.Random().nextInt(ROW);
+                y = new java.util.Random().nextInt(COLUMNS);
+            } while (matrix[x][y] != 0);
+            matrix[x][y] = i;
         }
     }
 
@@ -54,7 +75,7 @@ public class AdventCalendar {
     public void eat() {
         int minor = Integer.MAX_VALUE, x = 0, y = 0;
 
-        for (int i = 0; i < ROWS; i++) {
+        for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUMNS; j++) {
                 if (matrix[i][j] < minor && matrix[i][j] != 0) {
                     minor = matrix[i][j];
@@ -75,27 +96,16 @@ public class AdventCalendar {
     public boolean christmasIsHere() {
         boolean isChristmas = true;
 
-        for (int i = 0; i < ROWS && isChristmas; i++) {
+        for (int i = 0; i < ROW && isChristmas; i++) {
             for (int j = 0; j < COLUMNS && isChristmas; j++) {
                 if (matrix[i][j] != 0) {
                     isChristmas = false;
                 }
             }
         }
-
-        return isChristmas;
-    }
-
-    public static void main(String[] args) {
-        AdventCalendar calendar = new AdventCalendar();
-        calendar.fill();
-        System.out.println("Este es el calendario: ");
-        calendar.show();
-
-        while (!calendar.christmasIsHere()){
-            System.out.println("Voy a comer uno");
-            calendar.eat();
-            calendar.show();
+        if(isChristmas){
+            System.out.println("Feliz Navidad!");
         }
+        return isChristmas;
     }
 }
