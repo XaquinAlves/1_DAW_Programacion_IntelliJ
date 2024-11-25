b38785b8fb7992a65ee8251bd8be0c741bd3e09f
XaquinAlves <xaquinalves@gmail.com>
2024-11-25 14:17:07 +0100

    First Commit(actv3.8)

--- UD3/actividade8apt1/WaterBlocks.java
+++ UD3/actividade8apt1/WaterBlocks.java
@@ -2,21 +2,20 @@ package UD3.actividade8apt1;
 
 public class WaterBlocks {
 
-    public static int calcularAgua(int[] bloques){
+    public static int calcularAgua(int[] bloques) {
         int waterBloks = 0;
 
-        for (int i = 1; i < bloques.length-1; i++) {
-            int previous = bloques[i-1];
-            int next = bloques[i+1];
-            if(bloques[i] < previous && next > 0){
+        for (int i = 1; i < bloques.length - 1; i++) {
+            int previous = bloques[i - 1];
+            int next = bloques[i + 1];
+            if (bloques[i] < previous && next > 0) {
                 waterBloks += previous - bloques[i];
-            }else if(bloques[i] == previous && i > 1){
-                    if(bloques[i-2] > bloques[i] && next >0){
-                        waterBloks += bloques[i-2] - bloques[i];
-                }
+            } else if (bloques[i] == previous && i > 1) {
+
             }
         }
-
         return waterBloks;
     }
 }
+
+
